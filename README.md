785fcbc6ffcb978a55c6b81136add981d9958d1e
Andy Li <andy@onthewings.net>
2021-09-04 13:50:59 +0800

    create k8s

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Example [direnv](https://github.com/direnv/direnv) `.envrc`:
 export AWS_ACCESS_KEY_ID=FIXME
 export AWS_SECRET_ACCESS_KEY=FIXME
 export AWS_DEFAULT_REGION=eu-west-1
+
+# Optional. Let us use `kubectl`.
+# https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/#set-the-kubeconfig-environment-variable
+export KUBECONFIG="$(pwd)/kubeconfig_haxe2021"
 ```
---
+++ eks.tf
@@ -0,0 +1,84 @@
+locals {
+  cluster_name = local.stack_name
+
+  # Let's use only one AZ, such that ebs can be reliably re-attached.
+  eks_subnets = [module.vpc.public_subnets[0]]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args = [
+      "token",
+      "-i", module.eks.cluster_id,
+      "-r", aws_iam_role.k8s-admin.arn, # comment this out when the eks module is not created yet
+    ]
+    env = {
+      # AWS_ACCESS_KEY_ID
+      # AWS_SECRET_ACCESS_KEY
+      # AWS_DEFAULT_REGION
+    }
+  }
+
+  experiments {
+    manifest_resource = true
+  }
+}
+
+# https://github.com/terraform-aws-modules/terraform-aws-eks
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.10.0"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.21"
+  subnets         = module.vpc.public_subnets
+  enable_irsa     = true
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups_launch_template = [{
+    name = "${local.cluster_name}-spot"
+    override_instance_types = [
+      "t3.medium", "t3a.medium", "t4g.medium", "t2.medium",
+    ]
+
+    asg_min_size                             = 1
+    asg_max_size                             = 4
+    asg_desired_capacity                     = 1
+    on_demand_base_capacity                  = 0
+    on_demand_percentage_above_base_capacity = 0
+    spot_allocation_strategy                 = "lowest-price"
+
+    public_ip = true
+    # ami_id           = "ami-0d2a3c8fac031047a" # avoid auto update
+    root_volume_type = "gp2"
+    subnets          = local.eks_subnets,
+
+  }]
+
+  map_roles = [
+    {
+      rolearn  = aws_iam_role.k8s-admin.arn
+      username = aws_iam_role.k8s-admin.name
+      groups   = ["system:masters"]
+    }
+  ]
+  kubeconfig_aws_authenticator_additional_args = [
+    "-r", aws_iam_role.k8s-admin.arn
+  ]
+
+  depends_on = [
+    module.vpc, # EKS needs a VPC with an Internet gateway, so the VPC module better be completely created
+  ]
+}
