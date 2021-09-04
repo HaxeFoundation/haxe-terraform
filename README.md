418a7c2e4a7eabab83b8b72cb3783fb49309262a
Andy Li <andy@onthewings.net>
2021-09-04 12:33:14 +0800

    install more tools on devcontainer

---
+++ README.md
@@ -0,0 +1,8 @@
+Example [direnv](https://github.com/direnv/direnv) `.envrc`:
+
+```sh
+# AWS credentials
+export AWS_ACCESS_KEY_ID=FIXME
+export AWS_SECRET_ACCESS_KEY=FIXME
+export AWS_DEFAULT_REGION=eu-west-1
+```
--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45"
+      version = "~> 3.56"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
