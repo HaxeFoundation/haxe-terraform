0e391b0d8f4636a535f849ec8cf7867a0d350ae2
Andy Li <andy@onthewings.net>
2021-10-12 23:38:29 +0800

    use a dns-validated acm cert for nekovm.org

--- acm.tf
+++ acm.tf
@@ -28,10 +28,24 @@ resource "aws_acm_certificate" "haxedevelop-org-us-east-1" {
 }
 
 resource "aws_acm_certificate" "nekovm-org-us-east-1" {
-  provider                  = aws.us-east-1
-  domain_name               = "nekovm.org"
-  validation_method         = "EMAIL"
+  provider          = aws.us-east-1
+  domain_name       = "nekovm.org"
+  validation_method = "EMAIL"
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_acm_certificate" "nekovm-org-us-east-1-dns" {
+  provider          = aws.us-east-1
+  domain_name       = "nekovm.org"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_acm_certificate_validation" "nekovm-org-us-east-1-dns" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.nekovm-org-us-east-1-dns.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm-nekovm-org-us-east-1-dns : record.fqdn]
 }
--- cloudfront.tf
+++ cloudfront.tf
@@ -425,7 +425,7 @@ resource "aws_cloudfront_distribution" "nekovm-org" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.nekovm-org-us-east-1.arn
+    acm_certificate_arn      = aws_acm_certificate.nekovm-org-us-east-1-dns.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
