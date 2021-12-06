1ee250eccc1d111abf54bdcc15bc83f199c3962e
Andy Li <andy@onthewings.net>
2021-12-06 23:00:16 +0800

    upgrade terraform-aws-modules/eks/aws to 17.24.0

--- eks.tf
+++ eks.tf
@@ -62,7 +62,7 @@ provider "helm" {
 # https://github.com/terraform-aws-modules/terraform-aws-eks
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "17.10.0"
+  version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
   subnets         = module.vpc.public_subnets
