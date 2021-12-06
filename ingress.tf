b254166e6ff90896d5a12171fa2dd645760c742b
Andy Li <andy@onthewings.net>
2021-12-06 18:04:37 +0800

    upgrade ingress-nginx helm chart to 4.0.12

--- ingress.tf
+++ ingress.tf
@@ -7,7 +7,7 @@ resource "helm_release" "ingress-nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "4.0.5"
+  version    = "4.0.12"
 
   values = [
     yamlencode({
