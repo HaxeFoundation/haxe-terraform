11eac9d41a1fb5319afc5d0617d681900c169bb2
Andy Li <andy@onthewings.net>
2022-01-11 15:01:25 +0800

    add nginx-request-handling-performance grafana dashboard

--- grafana/dashboard-nginx.tf
+++ grafana/dashboard-nginx.tf
@@ -3,3 +3,9 @@ resource "grafana_dashboard" "nginx" {
   config_json = file("${path.module}/dashboard-nginx.json")
   overwrite   = true
 }
+
+resource "grafana_dashboard" "nginx-request-handling-performance" {
+  # Source: https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/grafana/dashboards/request-handling-performance.json
+  config_json = file("${path.module}/dashboard-nginx-request-handling-performance.json")
+  overwrite   = true
+}
