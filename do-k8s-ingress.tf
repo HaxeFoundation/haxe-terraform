8b0ea4d68ab0197170b7c6e83cd4bec0bcaf1cee
Andy Li <andy@onthewings.net>
2022-01-12 14:44:24 +0800

    add use-forwarded-headers for the DO ingress nginx
    because it's behind a load-balancer, and the haxelib server apache check the headers to decide whether force https

--- do-k8s-ingress.tf
+++ do-k8s-ingress.tf
@@ -23,6 +23,7 @@ resource "helm_release" "do-ingress-nginx" {
           "default" : true,
         },
         "config" : {
+          "use-forwarded-headers" : "true",
           "use-proxy-protocol" : "true",
           "proxy-body-size" : "256m",
           "http-snippet" : "proxy_cache_path /tmp/nginx_my_cache levels=1:2 keys_zone=mycache:2m use_temp_path=off max_size=2g inactive=48h;"
