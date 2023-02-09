fdd40ba2e583838239dbde3a480a9406819259a4
Andy Li <andy@onthewings.net>
2023-02-09 22:20:16 +0000

    avoid having duplicated resources for domain validation dns records

--- aws-route53.tf
+++ aws-route53.tf
@@ -12,36 +12,36 @@ resource "aws_route53_zone" "nekovm-org" {
 
 resource "aws_route53_record" "acm-haxe-org-us-east-1-dns" {
   for_each = {
-    for dvo in aws_acm_certificate.haxe-org-us-east-1-dns.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.haxe-org-us-east-1-dns.domain_validation_options :
+    dvo.resource_record_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
-    }
+    }...
   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.haxe-org.zone_id
+  name    = each.value[0].name
+  records = [each.value[0].record]
+  ttl     = 60
+  type    = each.value[0].type
+  zone_id = aws_route53_zone.haxe-org.zone_id
 }
 
 resource "aws_route53_record" "acm-nekovm-org-us-east-1-dns" {
   for_each = {
-    for dvo in aws_acm_certificate.nekovm-org-us-east-1-dns.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.nekovm-org-us-east-1-dns.domain_validation_options :
+    dvo.resource_record_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
-    }
+    }...
   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.nekovm-org.zone_id
+  name    = each.value[0].name
+  records = [each.value[0].record]
+  ttl     = 60
+  type    = each.value[0].type
+  zone_id = aws_route53_zone.nekovm-org.zone_id
 }
 
 resource "aws_route53_record" "haxe-org" {
