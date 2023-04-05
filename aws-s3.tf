f626b9f27de038884b4695c1f1d43056bc9cfd0c
Andy Li <andy@onthewings.net>
2023-04-05 16:48:39 +0100

    import s3 buckets

--- aws-s3.tf
+++ aws-s3.tf
@@ -11,7 +11,7 @@ module "s3_bucket_terraform" {
 
   lifecycle_rule = [
     {
-      id = "noncurrent"
+      id      = "noncurrent"
       enabled = true
       noncurrent_version_transition = [
         {
@@ -24,4 +24,48 @@ module "s3_bucket_terraform" {
       }
     }
   ]
+}
+
+resource "aws_s3_bucket" "api-haxe-org" {
+  bucket = "api.haxe.org"
+}
+
+resource "aws_s3_bucket" "aws-sdk-neko" {
+  bucket = "aws-sdk-neko"
+}
+
+resource "aws_s3_bucket" "blog-haxe-org" {
+  bucket = "blog.haxe.org"
+}
+
+resource "aws_s3_bucket" "build-haxe-org" {
+  bucket = "build.haxe.org"
+}
+
+resource "aws_s3_bucket" "code-haxe-org" {
+  bucket = "code.haxe.org"
+}
+
+resource "aws_s3_bucket" "haxe-blog" {
+  bucket = "haxe-blog"
+}
+
+resource "aws_s3_bucket" "haxe-org" {
+  bucket = "haxe.org"
+}
+
+resource "aws_s3_bucket" "nekovm-org" {
+  bucket = "nekovm.org"
+}
+
+resource "aws_s3_bucket" "staging-haxe-org" {
+  bucket = "staging.haxe.org"
+}
+
+resource "aws_s3_bucket" "www-haxe-org" {
+  bucket = "www.haxe.org"
+}
+
+resource "aws_s3_bucket" "www-haxedevelop-org" {
+  bucket = "www.haxedevelop.org"
 }
